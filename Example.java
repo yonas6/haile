3f126a0c3757c6f3d24db5f752329a6ca91e6073
haile <hyonas67@gmail.com>
2017-09-11 01:10:55 -0400

    modified

---
+++ Example.java
@@ -0,0 +1,15 @@
+package webDriverpackage;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age;
+age = 20; //the declaration of the variable
+
+System.out.println("The variable contains:" + age );
+
+//todo auto-generated method
+	}
+
+}
